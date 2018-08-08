01ae0df642c2ad96456b3e00238258d2757bfda5
Sava Mikalacki <mikalackis@gmail.com>
2018-08-08 12:16:00 +0200

    Introduce Ariel product to build
    Change-Id: If6e34b5dec60b355e0eb7dee7fce5e8314774afd

--- envsetup.sh
+++ envsetup.sh
@@ -139,7 +139,11 @@ function check_product()
         LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
         export BUILD_NUMBER=$( (date +%s%N ; echo $LINEAGE_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
-        LINEAGE_BUILD=
+        if (echo -n $1 | grep -q -e "^ariel_") ; then
+           LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^ariel_//g')
+        else
+           LINEAGE_BUILD=
+        fi
     fi
     export LINEAGE_BUILD
 
